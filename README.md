53d4713b60f4425cca5c545ab2af3b1f512f946c
Wadad Parker <wadadparker5@gmail.com>
2024-07-07 18:30:38 +0530

    feat: emit socket events
    Emit socket events from client into the server and console them. This way multiple users can be connected to the socket server

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - We will first setup a normal repo with socket.io & express.
 - - Use express to create a web server & create a server instance
 - - use createServer function of node:http 
+- Setup Socket.io
+- - import Server from socket.io library
+- - Create a new instance of websocket server io
+- - From index.html, setup a client socket instance to emit message from client to the socket server (now that's cool)
--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import express from 'express';
 import {createServer} from "node:http";
+import { fileURLToPath } from 'node:url';
+import { dirname , join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+// This io is actually a Wrapper around our server instance so that it listens to all requests going to this server
 
-app.get('/', (req,res) => {
-    res.send(`<h1>Hello Za Worldo</h1>`);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// This dirname is just so we can directly send html file as a response to that API call
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
 });
+
+io.on('connection', (socket) => {
+    console.log('Connection established');
+
+    socket.on('chat message', (msg) => {
+        console.log('received message',msg);
+        io.emit('chat message',msg)
+    })
+    socket.on('disconnect', () => {
+        console.log('User disconnected!');
+    } )
+})
 
 server.listen(3000, () => {
     console.log('server runing at http://localhost:3000');
